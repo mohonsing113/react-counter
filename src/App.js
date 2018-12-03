2af4ee5bee3ae938b281a7d25c49cc7db0ec0078
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-03 15:02:50 +0800

    init with copy of react-demo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state={number: 0}
+  render() {
+    return (
+      <div>
+        <button onClick={()=>{this.setState({number: this.state.number+1})}}>+</button>
+        <span>{this.state.number}</span>
+      </div>
+    );
+  }
+}
+
+export default App;
